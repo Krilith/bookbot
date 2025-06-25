bf2ce1dfa55a019eacbf782ff4f06a08b15496aa
Krilith <jasonvank@yahoo.com>
2025-06-25 15:08:02 -0400

    Added arguments to link path in main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3 -O
-#starting script, contains debug options, add -O when script is in production
+#!/usr/bin/python3 
 
+import sys
 from stats import get_num_words, sort_output, get_num_letters
 
-if __debug__:
-    print("---start")
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 
 #list of global variables
 
@@ -13,38 +15,29 @@ file_contents = ""
 #Functions
 
 def get_book_text(book):
+
     with open(book) as f:
         file_contents = f.read()
     return(file_contents)
 
-
-
-
-
 def main():
     
-    text = get_book_text("./books/frankenstein.txt")
+    text = get_book_text(sys.argv[1])
     total = get_num_words(text)
     letters = get_num_letters(text)
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {total} total words")
     print("--------- Character Count -------")
+
     clean = sort_output(letters)
+
     for key, value in clean.items():
         print(f"{key}: {value}")
-
 
     print("============= END ===============")
     pass
 
-
-
-
-
-if __debug__:
-    print("---call main")
 main()
-if __debug__:
-    print("---end")
--- stats.py
+++ stats.py
@@ -1,27 +1,29 @@
 #counts word total from string
 def get_num_words(text):
+
     word_list = text.split()
     words = len(word_list)
     return words
 
 #counts character total from sting
 def get_num_letters(text):
+
     text_counts = {}
     text = text.lower()
     letters = list(text)
     unique_list = set(letters)
 
-
     for each in unique_list:
         if each.isalpha() == True:
             ammount = text.count(each)
             text_counts[each] = ammount
+
     return text_counts
 
 #sorts the output 
 def sort_output(text):
+
     sorted_items = sorted(text.items(), key=lambda item: item[1], reverse = True)
     sorted_dict = dict(sorted_items)
 
     return(sorted_dict)
-
